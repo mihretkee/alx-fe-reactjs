d5a3767da4957513b0f1c50f3bd8a38a3ed6c0b0
mihret kee <1keemem1@gmail.com>
2025-07-19 16:29:15 +0300

    Add UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,14 +7,22 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';  
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header /> {/* <-- required */}
-      <MainContent /> {/* <-- required */}
+      <Header />
+      <MainContent />
+
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
       <WelcomeMessage />
 
       <div>
@@ -35,11 +43,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
 
-      <Footer /> {/* <-- required */}
+      <Footer />
     </>
   );
 }
